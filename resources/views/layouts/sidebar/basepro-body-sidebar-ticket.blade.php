fffec34a6f2f807d72b074a3d00b872f94709455
Lucas Acosta <lucasaacosta1995@gmail.com>
2025-07-02 21:49:21 -0300

    menu para tickets

---
+++ resources/views/layouts/sidebar/basepro-body-sidebar-ticket.blade.php
@@ -0,0 +1,42 @@
+@if( Session::has('ticket') || Session::has('reparacion') )
+<li>
+    <a href="#">
+        <i class="metismenu-icon {{Session::get('icon_roottickets','pe-7s-ticket')}} "></i>
+        Tickets
+        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+    </a>
+    <ul>
+        
+        @php
+            $url = "ticket";
+            $session_icon = "icon_".$url;
+            $iconforced="";
+            if(Session::has($session_icon)){
+                $iconforced="iconforced";
+            }
+        @endphp
+        <li>
+            <a href="{{ Session::get('speedup_url').'ticket' }}">
+                <i class="metismenu-icon {{Session::get($session_icon,'pe-7s-news-paper')}} {{$iconforced}}"></i>Listar Tickets
+            </a>
+        </li>
+
+        @php
+            $url = "ticketcerrados";
+            $session_icon = "icon_".$url;
+            $iconforced="";
+            if(Session::has($session_icon)){
+                $iconforced="iconforced";
+            }
+        @endphp
+
+        <li>
+            <a href="{{ Session::get('speedup_url').'indexCerrados' }}">
+                <i class="metismenu-icon {{Session::get($session_icon,'pe-7s-news-paper')}} {{$iconforced}}"></i>Listar Tickets Cerrados
+            </a>
+        </li>
+        
+    </ul>
+    
+</li>
+@endif
