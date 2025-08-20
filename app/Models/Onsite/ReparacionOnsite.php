9c93a8ba3cc5127318ea771ae6282eb0773622e6
Adan Gatica <adanrobertogatica@gmail.com>
2025-08-19 21:06:48 -0300

    ONS 376

--- app/Models/Onsite/ReparacionOnsite.php
+++ app/Models/Onsite/ReparacionOnsite.php
@@ -79,7 +79,10 @@ class ReparacionOnsite extends Model
 
 		'sistema_onsite_id',
 		'reparacion_onsite_puesta_marcha_id',
-		'solicitud_tipo_id'
+		'solicitud_tipo_id',
+
+		'justificacion',
+		'log'
 
 	];
 
