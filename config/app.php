f9f36fe043a36d356ca12cc49f609d1f70498f08
Adan Gatica <adanrobertogatica@gmail.com>
2025-09-02 19:41:04 -0300

    Test a Produccion fix html

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -236,7 +237,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Mapper'=> Cornford\Googlmapper\Facades\MapperFacade::class,
-      
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
