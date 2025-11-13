b3e0fc1d3a8562a8cb37cd7495ded8e4d339c716
Adan Gatica <adanrobertogatica@gmail.com>
2025-11-13 18:14:38 -0300

    Adan G- Mejoras query endpoint api filter rep by case

--- config/queries.php
+++ config/queries.php
@@ -1,273 +1,131 @@
 <?php
 
 return [
-    'view_reparaciones_onsite'=>"SELECT
-	`r`.`id` AS `id`,
-	min(`r`.`company_id`) AS `company_id`,
-	min(`r`.`clave`) AS `clave`,
-	min(`r`.`id_empresa_onsite`) AS `id_empresa_onsite`,
-	min(`e`.`nombre`) AS `empresa_nombre`,
-	min(`r`.`sucursal_onsite_id`) AS `sucursal_id`,
-	min(`s`.`razon_social`) AS `razon_social`,
-	min(`s`.`direccion`) AS `direccion`,
-	min(`s`.`telefono_contacto`) AS `tel_contacto`,
-	min(`r`.`id_terminal`) AS `id_terminal`,
-	min(`t`.`marca`) AS `marca`,
-	min(`t`.`modelo`) AS `modelo`,
-	min(`t`.`serie`) AS `serie`,
-	min(`t`.`rotulo`) AS `rotulo`,
-	min(`s`.`localidad_onsite_id`) AS `localidad_onsite_id`,
-	min(`l`.`localidad`) AS `localidad`,
-	min(`l`.`id_provincia`) AS `id_provincia`,
-	min(`p`.`nombre`) AS `provincia_nombre`,
-	min(`l`.`localidad_estandard`) AS `localidad_estandard`,
-	min(`l`.`codigo`) AS `codigo`,
-	min(`l`.`km`) AS `km`,
-	min(`l`.`id_nivel`) AS `id_nivel`,
-	min(`n`.`nombre`) AS `nivel_nombre`,
-	min(`l`.`atiende_desde`) AS `atiende_desde`,
-	min(`l`.`id_usuario_tecnico`) AS `id_usuario_tecnico`,
-	min(`u`.`name`) AS `usuario`,
-	min(`r`.`tarea`) AS `tarea`,
-	min(`r`.`tarea_detalle`) AS `tarea_detalle`,
-	min(`r`.`id_tipo_servicio`) AS `id_tipo_serv`,
-	min(`ts`.`nombre`) AS `ts_nombre`,
-	min(`r`.`id_estado`) AS `id_estado`,
-	min(`es`.`nombre`) AS `estado_nombre`,
-	min(`es`.`activo`) AS `estado_activo`,
-	min(`r`.`fecha_ingreso`) AS `fecha_ingreso`,
-	min(
-		`r`.`observacion_ubicacion`
-	) AS `r_observacion_ubicacion`,
-	min(`r`.`id_tecnico_asignado`) AS `id_tecnico_asignado`,
-	min(`tec`.`name`) AS `tecnico`,
-	min(`r`.`informe_tecnico`) AS `r_informe_tecnico`,
-	min(`r`.`fecha_coordinada`) AS `r_fecha_coordinada`,
-	min(
-		`r`.`ventana_horaria_coordinada`
-	) AS `r_ventana_horaria_coordinada`,
-	min(
-		`r`.`fecha_registracion_coordinacion`
-	) AS `r_fecha_registracion_coordinacion`,
-	min(`r`.`fecha_notificado`) AS `r_fecha_notificado`,
-	min(`rv`.`fecha`) AS `primer_visita`,
-	min(`rv`.`fecha_vencimiento`) AS `fecha_vencimiento`,
-	min(`r`.`fecha_vencimiento`) AS `vencimiento`,
-	min(`r`.`fecha_cerrado`) AS `r_fecha_cerrado`,
-	min(`r`.`sla_status`) AS `r_sla_status`,
-	min(`r`.`sla_justificado`) AS `r_sla_justificado`,
-	min(`r`.`monto`) AS `r_monto`,
-	min(`r`.`monto_extra`) AS `r_monto_extra`,
-	min(`r`.`liquidado_proveedor`) AS `r_liquidado_proveedor`,
-	min(
-		`r`.`nro_factura_proveedor`
-	) AS `r_nro_factura_proveedor`,
-	min(`rd`.`tipo_conexion_local`) AS `rd_tipo_conexion_local`,
-	min(
-		`rd`.`tipo_conexion_proveedor`
-	) AS `MIN(``rd``.``tipo_conexion_proveedor``)`,
-	min(`rd`.`cableado`) AS `rd_cableado`,
-	min(
-		`rd`.`cableado_cantidad_metros`
-	) AS `MIN(``rd``.``cableado_cantidad_metros``)`,
-	min(
-		`rd`.`cableado_cantidad_fichas`
-	) AS `MIN(``rd``.``cableado_cantidad_fichas``)`,
-	min(`rd`.`instalacion_cartel`) AS `MIN(``rd``.``instalacion_cartel``)`,
-	min(
-		`rd`.`instalacion_cartel_luz`
-	) AS `MIN(``rd``.``instalacion_cartel_luz``)`,
-	min(`r`.`instalacion_buzon`) AS `MIN(``r``.``instalacion_buzon``)`,
-	min(
-		`r`.`cantidad_horas_trabajo`
-	) AS `MIN(``r``.``cantidad_horas_trabajo``)`,
-	min(
-		`r`.`requiere_nueva_visita`
-	) AS `MIN(``r``.``requiere_nueva_visita``)`,
-	min(
-		`rd`.`codigo_activo_nuevo1`
-	) AS `MIN(``rd``.``codigo_activo_nuevo1``)`,
-	min(
-		`rd`.`codigo_activo_retirado1`
-	) AS `rd_codigo_activo_retirado1`,
-	min(
-		`rd`.`codigo_activo_descripcion1`
-	) AS `rd_codigo_activo_descripcion1`,
-	min(
-		`rd`.`codigo_activo_nuevo2`
-	) AS `MIN(``rd``.``codigo_activo_nuevo2``)`,
-	min(
-		`rd`.`codigo_activo_retirado2`
-	) AS `MIN(``rd``.``codigo_activo_retirado2``)`,
-	min(
-		`rd`.`codigo_activo_descripcion2`
-	) AS `MIN(``rd``.``codigo_activo_descripcion2``)`,
-	min(
-		`rd`.`codigo_activo_nuevo3`
-	) AS `MIN(``rd``.``codigo_activo_nuevo3``)`,
-	min(
-		`rd`.`codigo_activo_retirado3`
-	) AS `MIN(``rd``.``codigo_activo_retirado3``)`,
-	min(
-		`rd`.`codigo_activo_descripcion3`
-	) AS `MIN(``rd``.``codigo_activo_descripcion3``)`,
-	min(
-		`rd`.`codigo_activo_nuevo4`
-	) AS `MIN(``rd``.``codigo_activo_nuevo4``)`,
-	min(
-		`rd`.`codigo_activo_retirado4`
-	) AS `MIN(``rd``.``codigo_activo_retirado4``)`,
-	min(
-		`rd`.`codigo_activo_descripcion4`
-	) AS `MIN(``rd``.``codigo_activo_descripcion4``)`,
-	min(
-		`rd`.`codigo_activo_nuevo5`
-	) AS `MIN(``rd``.``codigo_activo_nuevo5``)`,
-	min(
-		`rd`.`codigo_activo_retirado5`
-	) AS `MIN(``rd``.``codigo_activo_retirado5``)`,
-	min(
-		`rd`.`codigo_activo_descripcion5`
-	) AS `MIN(``rd``.``codigo_activo_descripcion5``)`,
-	min(
-		`rd`.`codigo_activo_nuevo6`
-	) AS `MIN(``rd``.``codigo_activo_nuevo6``)`,
-	min(
-		`rd`.`codigo_activo_retirado6`
-	) AS `MIN(``rd``.``codigo_activo_retirado6``)`,
-	min(
-		`rd`.`codigo_activo_descripcion6`
-	) AS `MIN(``rd``.``codigo_activo_descripcion6``)`,
-	min(
-		`rd`.`codigo_activo_nuevo7`
-	) AS `MIN(``rd``.``codigo_activo_nuevo7``)`,
-	min(
-		`rd`.`codigo_activo_retirado7`
-	) AS `MIN(``rd``.``codigo_activo_retirado7``)`,
-	min(
-		`rd`.`codigo_activo_descripcion7`
-	) AS `MIN(``rd``.``codigo_activo_descripcion7``)`,
-	min(
-		`rd`.`codigo_activo_nuevo8`
-	) AS `MIN(``rd``.``codigo_activo_nuevo8``)`,
-	min(
-		`rd`.`codigo_activo_retirado8`
-	) AS `MIN(``rd``.``codigo_activo_retirado8``)`,
-	min(
-		`rd`.`codigo_activo_descripcion8`
-	) AS `MIN(``rd``.``codigo_activo_descripcion8``)`,
-	min(
-		`rd`.`codigo_activo_nuevo9`
-	) AS `MIN(``rd``.``codigo_activo_nuevo9``)`,
-	min(
-		`rd`.`codigo_activo_retirado9`
-	) AS `MIN(``rd``.``codigo_activo_retirado9``)`,
-	min(
-		`rd`.`codigo_activo_descripcion9`
-	) AS `MIN(``rd``.``codigo_activo_descripcion9``)`,
-	min(
-		`rd`.`codigo_activo_nuevo10`
-	) AS `MIN(``rd``.``codigo_activo_nuevo10``)`,
-	min(
-		`rd`.`codigo_activo_retirado10`
-	) AS `MIN(``rd``.``codigo_activo_retirado10``)`,
-	min(
-		`rd`.`codigo_activo_descripcion10`
-	) AS `MIN(``rd``.``codigo_activo_descripcion10``)`,
-	min(
-		`rd`.`modem_3g_4g_sim_nuevo`
-	) AS `MIN(``rd``.``modem_3g_4g_sim_nuevo``)`,
-	min(
-		`rd`.`modem_3g_4g_sim_retirado`
-	) AS `MIN(``rd``.``modem_3g_4g_sim_retirado``)`,
-	min(`r`.`firma_cliente`) AS `r_firma_cliente`,
-	min(`r`.`aclaracion_cliente`) AS `r_aclaracion_cliente`,
-	min(`r`.`firma_tecnico`) AS `r_firma_tecnico`,
-	min(`r`.`aclaracion_tecnico`) AS `r_aclaracion_tecnico`,
-	min(`r`.`created_at`) AS `created_at`
+    'view_reparaciones_onsite' => "SELECT
+    `r`.`id` AS `id`,
+    `r`.`company_id` AS `company_id`,
+    `r`.`clave` AS `clave`,
+    `r`.`id_empresa_onsite` AS `id_empresa_onsite`,
+    `e`.`nombre` AS `empresa_nombre`,
+    `r`.`sucursal_onsite_id` AS `sucursal_id`,
+    `s`.`razon_social` AS `razon_social`,
+    `s`.`direccion` AS `direccion`,
+    `s`.`telefono_contacto` AS `tel_contacto`,
+    `r`.`id_terminal` AS `id_terminal`,
+    `t`.`marca` AS `marca`,
+    `t`.`modelo` AS `modelo`,
+    `t`.`serie` AS `serie`,
+    `t`.`rotulo` AS `rotulo`,
+    `s`.`localidad_onsite_id` AS `localidad_onsite_id`,
+    `l`.`localidad` AS `localidad`,
+    `l`.`id_provincia` AS `id_provincia`,
+    `p`.`nombre` AS `provincia_nombre`,
+    `l`.`localidad_estandard` AS `localidad_estandard`,
+    `l`.`codigo` AS `codigo`,
+    `l`.`km` AS `km`,
+    `l`.`id_nivel` AS `id_nivel`,
+    `n`.`nombre` AS `nivel_nombre`,
+    `l`.`atiende_desde` AS `atiende_desde`,
+    `l`.`id_usuario_tecnico` AS `id_usuario_tecnico`,
+    `u`.`name` AS `usuario`,
+    `r`.`tarea` AS `tarea`,
+    `r`.`tarea_detalle` AS `tarea_detalle`,
+    `r`.`id_tipo_servicio` AS `id_tipo_serv`,
+    `ts`.`nombre` AS `ts_nombre`,
+    `r`.`id_estado` AS `id_estado`,
+    `es`.`nombre` AS `estado_nombre`,
+    `es`.`activo` AS `estado_activo`,
+    `r`.`fecha_ingreso` AS `fecha_ingreso`,
+    `r`.`observacion_ubicacion` AS `r_observacion_ubicacion`,
+    `r`.`id_tecnico_asignado` AS `id_tecnico_asignado`,
+    `tec`.`name` AS `tecnico`,
+    `r`.`informe_tecnico` AS `r_informe_tecnico`,
+    `r`.`fecha_coordinada` AS `r_fecha_coordinada`,
+    `r`.`ventana_horaria_coordinada` AS `r_ventana_horaria_coordinada`,
+    `r`.`fecha_registracion_coordinacion` AS `r_fecha_registracion_coordinacion`,
+    `r`.`fecha_notificado` AS `r_fecha_notificado`,
+    `rv`.`primer_visita` AS `primer_visita`,
+    `rv`.`fecha_vencimiento` AS `fecha_vencimiento`,
+    `r`.`fecha_vencimiento` AS `vencimiento`,
+    `r`.`fecha_cerrado` AS `r_fecha_cerrado`,
+    `r`.`sla_status` AS `r_sla_status`,
+    `r`.`sla_justificado` AS `r_sla_justificado`,
+    `r`.`monto` AS `r_monto`,
+    `r`.`monto_extra` AS `r_monto_extra`,
+    `r`.`liquidado_proveedor` AS `r_liquidado_proveedor`,
+    `r`.`nro_factura_proveedor` AS `r_nro_factura_proveedor`,
+    
+    `rd`.`tipo_conexion_local` AS `rd_tipo_conexion_local`,
+    `rd`.`tipo_conexion_proveedor` AS `rd_tipo_conexion_proveedor`,
+    `rd`.`cableado` AS `rd_cableado`,
+    `rd`.`cableado_cantidad_metros` AS `rd_cableado_cantidad_metros`,
+    `rd`.`cableado_cantidad_fichas` AS `rd_cableado_cantidad_fichas`,
+    `rd`.`instalacion_cartel` AS `rd_instalacion_cartel`,
+    `rd`.`instalacion_cartel_luz` AS `rd_instalacion_cartel_luz`,
+    `r`.`instalacion_buzon` AS `r_instalacion_buzon`,
+    `r`.`cantidad_horas_trabajo` AS `r_cantidad_horas_trabajo`,
+    `r`.`requiere_nueva_visita` AS `r_requiere_nueva_visita`,
+    
+    `rd`.`codigo_activo_nuevo1` AS `rd_codigo_activo_nuevo1`,
+    `rd`.`codigo_activo_retirado1` AS `rd_codigo_activo_retirado1`,
+    `rd`.`codigo_activo_descripcion1` AS `rd_codigo_activo_descripcion1`,
+    `rd`.`codigo_activo_nuevo2` AS `rd_codigo_activo_nuevo2`,
+    `rd`.`codigo_activo_retirado2` AS `rd_codigo_activo_retirado2`,
+    `rd`.`codigo_activo_descripcion2` AS `rd_codigo_activo_descripcion2`,
+    `rd`.`codigo_activo_nuevo3` AS `rd_codigo_activo_nuevo3`,
+    `rd`.`codigo_activo_retirado3` AS `rd_codigo_activo_retirado3`,
+    `rd`.`codigo_activo_descripcion3` AS `rd_codigo_activo_descripcion3`,
+    `rd`.`codigo_activo_nuevo4` AS `rd_codigo_activo_nuevo4`,
+    `rd`.`codigo_activo_retirado4` AS `rd_codigo_activo_retirado4`,
+    `rd`.`codigo_activo_descripcion4` AS `rd_codigo_activo_descripcion4`,
+    `rd`.`codigo_activo_nuevo5` AS `rd_codigo_activo_nuevo5`,
+    `rd`.`codigo_activo_retirado5` AS `rd_codigo_activo_retirado5`,
+    `rd`.`codigo_activo_descripcion5` AS `rd_codigo_activo_descripcion5`,
+    `rd`.`codigo_activo_nuevo6` AS `rd_codigo_activo_nuevo6`,
+    `rd`.`codigo_activo_retirado6` AS `rd_codigo_activo_retirado6`,
+    `rd`.`codigo_activo_descripcion6` AS `rd_codigo_activo_descripcion6`,
+    `rd`.`codigo_activo_nuevo7` AS `rd_codigo_activo_nuevo7`,
+    `rd`.`codigo_activo_retirado7` AS `rd_codigo_activo_retirado7`,
+    `rd`.`codigo_activo_descripcion7` AS `rd_codigo_activo_descripcion7`,
+    `rd`.`codigo_activo_nuevo8` AS `rd_codigo_activo_nuevo8`,
+    `rd`.`codigo_activo_retirado8` AS `rd_codigo_activo_retirado8`,
+    `rd`.`codigo_activo_descripcion8` AS `rd_codigo_activo_descripcion8`,
+    `rd`.`codigo_activo_nuevo9` AS `rd_codigo_activo_nuevo9`,
+    `rd`.`codigo_activo_retirado9` AS `rd_codigo_activo_retirado9`,
+    `rd`.`codigo_activo_descripcion9` AS `rd_codigo_activo_descripcion9`,
+    `rd`.`codigo_activo_nuevo10` AS `rd_codigo_activo_nuevo10`,
+    `rd`.`codigo_activo_retirado10` AS `rd_codigo_activo_retirado10`,
+    `rd`.`codigo_activo_descripcion10` AS `rd_codigo_activo_descripcion10`,
+    `rd`.`modem_3g_4g_sim_nuevo` AS `rd_modem_3g_4g_sim_nuevo`,
+    `rd`.`modem_3g_4g_sim_retirado` AS `rd_modem_3g_4g_sim_retirado`,
+    
+    `r`.`firma_cliente` AS `r_firma_cliente`,
+    `r`.`aclaracion_cliente` AS `r_aclaracion_cliente`,
+    `r`.`firma_tecnico` AS `r_firma_tecnico`,
+    `r`.`aclaracion_tecnico` AS `r_aclaracion_tecnico`,
+    `r`.`created_at` AS `created_at`
 FROM
-	(
-		(
-			(
-				(
-					(
-						(
-							(
-								(
-									(
-										(
-											(
-												(
-													`reparaciones_onsite` `r`
-													LEFT JOIN `sucursales_onsite` `s` ON (
-														(
-															`r`.`sucursal_onsite_id` = `s`.`id`
-														)
-													)
-												)
-												LEFT JOIN `empresas_onsite` `e` ON (
-													(
-														`r`.`id_empresa_onsite` = `e`.`id`
-													)
-												)
-											)
-											LEFT JOIN `terminales_onsite` `t` ON (
-												(
-													`r`.`id_terminal` = `t`.`nro`
-												)
-											)
-										)
-										LEFT JOIN `localidades_onsite` `l` ON (
-											(
-												`s`.`localidad_onsite_id` = `l`.`id`
-											)
-										)
-									)
-									LEFT JOIN `provincias` `p` ON (
-										(
-											`l`.`id_provincia` = `p`.`id`
-										)
-									)
-								)
-								LEFT JOIN `niveles_onsite` `n` ON ((`l`.`id_nivel` = `n`.`id`))
-							)
-							LEFT JOIN `users` `u` ON (
-								(
-									`l`.`id_usuario_tecnico` = `u`.`id`
-								)
-							)
-						)
-						LEFT JOIN `tipos_servicios_onsite` `ts` ON (
-							(
-								`r`.`id_tipo_servicio` = `ts`.`id`
-							)
-						)
-					)
-					LEFT JOIN `estados_onsite` `es` ON (
-						(`r`.`id_estado` = `es`.`id`)
-					)
-				)
-				LEFT JOIN `users` `tec` ON (
-					(
-						`r`.`id_tecnico_asignado` = `tec`.`id`
-					)
-				)
-			)
-			LEFT JOIN `reparaciones_visitas` `rv` ON (
-				(
-					`rv`.`reparacion_id` = `r`.`id`
-				)
-			)
-		)
-		LEFT JOIN `reparaciones_detalle` `rd` ON (
-			(
-				`rd`.`reparacion_id` = `r`.`id`
-			)
-		)
-	)
+    `reparaciones_onsite` `r`
+    LEFT JOIN `sucursales_onsite` `s` ON (`r`.`sucursal_onsite_id` = `s`.`id`)
+    LEFT JOIN `empresas_onsite` `e` ON (`r`.`id_empresa_onsite` = `e`.`id`)
+    LEFT JOIN `terminales_onsite` `t` ON (`r`.`id_terminal` = `t`.`nro`)
+    LEFT JOIN `localidades_onsite` `l` ON (`s`.`localidad_onsite_id` = `l`.`id`)
+    LEFT JOIN `provincias` `p` ON (`l`.`id_provincia` = `p`.`id`)
+    LEFT JOIN `niveles_onsite` `n` ON (`l`.`id_nivel` = `n`.`id`)
+    LEFT JOIN `users` `u` ON (`l`.`id_usuario_tecnico` = `u`.`id`)
+    LEFT JOIN `tipos_servicios_onsite` `ts` ON (`r`.`id_tipo_servicio` = `ts`.`id`)
+    LEFT JOIN `estados_onsite` `es` ON (`r`.`id_estado` = `es`.`id`)
+    LEFT JOIN `users` `tec` ON (`r`.`id_tecnico_asignado` = `tec`.`id`)
+    LEFT JOIN (
+        SELECT
+            `reparacion_id`,
+            MIN(`fecha`) AS `primer_visita`,
+            MIN(`fecha_vencimiento`) AS `fecha_vencimiento`
+        FROM `reparaciones_visitas`
+        GROUP BY `reparacion_id`
+    ) `rv` ON (`rv`.`reparacion_id` = `r`.`id`)
+    LEFT JOIN `reparaciones_detalle` `rd` ON (`rd`.`reparacion_id` = `r`.`id`)
 WHERE `r`.`id` IN (%repIds%) AND `r`.`company_id` = %company_id%
-GROUP BY
-	`r`.`id`
 ORDER BY `r`.`id` DESC
 LIMIT %offset%,%per_page%
 "
